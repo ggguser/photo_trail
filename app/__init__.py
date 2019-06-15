a3cb481d376e29aaf0774860d891cab32d436dbf
Lagunov-PRO-Mac <alexey@lagunov.pro>
2019-06-15 18:18:52 +0300

    Added create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,18 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)  # настроит всё за меня
-login = LoginManager(app)
+db = SQLAlchemy()  # настроит всё за меня
+login = LoginManager()
 
 
-from app import routes, models
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    db.init_app(app)
+    login.init_app(app)
+
+    return app
+
+
+from app import models
+
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from app import app, db
+from app import db
 from app.forms import LoginForm
 from app.models import User
 

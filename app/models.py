423fafcf568e33adc609f3e484bd16fdfde220dc
Lagunov-PRO-Mac <alexey@lagunov.pro>
2019-06-15 15:02:53 +0300

    Added database and models.py

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from app.routes import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __str__(self):
+        return f'Issue #{self.id}: {self.name}'
+
+    def __repr__(self):
+        return f'Issue #{self.id}: {self.name}'

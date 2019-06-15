e8f92a0b6025caf17f4e17040d22a997221b69e1
Lagunov-PRO-Mac <alexey@lagunov.pro>
2019-06-15 17:24:25 +0300

    Added user login

--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-from app.routes import db
+from app.routes import db, login
 from datetime import datetime
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -47,3 +48,8 @@ class Photo(db.Model):
 
     def __repr__(self):
         return f'<Photo {self.name}>'
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,11 @@ import os
 import uuid
 
 from flask import Flask, render_template, request, url_for, redirect, flash
+from flask_login import current_user, login_user
 from flask_sqlalchemy import SQLAlchemy
 
 from app.forms import LoginForm
+from app.models import User
 
 app = Flask(__name__)
 app.config['IMAGE_DIR'] = os.path.join('static', 'photos')
@@ -24,9 +26,15 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 

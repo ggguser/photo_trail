d49d2ef9f13b5aa07aa9154ef3c0797e5847a217
Lagunov-PRO-Mac <alexey@lagunov.pro>
2019-06-15 16:22:35 +0300

    Added password hashing and checking

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from app.routes import db
 from datetime import datetime
 
@@ -8,6 +10,12 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     trails = db.relationship('Trail', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __str__(self):
         return f'Issue #{self.id}: {self.name}'

168dc1e705009c1a4520ac30106f3b6fa1d7ad24
Lagunov-PRO-Mac <alexey@lagunov.pro>
2019-06-15 15:24:34 +0300

    Model updated user/trail/photo

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app.routes import db
+from datetime import datetime
 
 
 class User(db.Model):
@@ -6,9 +7,34 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    trails = db.relationship('Trail', backref='author', lazy='dynamic')
 
     def __str__(self):
         return f'Issue #{self.id}: {self.name}'
 
     def __repr__(self):
         return f'Issue #{self.id}: {self.name}'
+
+
+class Trail(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    photos = db.relationship('Photo', backref='trail', lazy='dynamic')
+
+    def __repr__(self):
+        return f'<Trail {self.body}>'
+
+
+class Photo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(140))
+    # date = db.Column(db.String(140))  #  TODO: решить, как работать с датой и временем
+    lat = db.Column(db.Float)
+    lng = db.Column(db.Float)
+    timestamp = db.Column(db.DateTime, index=True)
+    trail_id = db.Column(db.Integer, db.ForeignKey('trail.id'))
+
+    def __repr__(self):
+        return f'<Photo {self.body}>'

9da9e8f49fc42caff449eec4eb11701260f5d30f
Lagunov-PRO-Mac <alexey@lagunov.pro>
2019-06-29 11:43:06 +0300

    Added env variable as db path

--- app/config.py
+++ app/config.py
@@ -8,5 +8,10 @@ load_dotenv(os.path.join(basedir, '.env'))
 class Config(object):
     IMAGE_DIR = os.path.join('app', 'static', 'photos')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'this_is_secret'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///phototrail.sqlite'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'phototrail.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class Messages():
+    pass

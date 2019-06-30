aa1cd52b29d45f2774237445d81ef8d37175d15c
Lagunov-PRO-Mac <alexey@lagunov.pro>
2019-06-30 12:40:36 +0300

    Divided the data from location

--- app/exif.py
+++ app/exif.py
@@ -21,9 +21,6 @@ def _get_if_exist(data, key):
 def _convert_to_degrees(value):
     """
     Helper function to convert the GPS coordinates stored in the EXIF to degrees in float format
-    :param value:
-    :type value: exifread.utils.Ratio
-    :rtype: float
     """
     d = float(value.values[0].num) / float(value.values[0].den)
     m = float(value.values[1].num) / float(value.values[1].den)
@@ -32,34 +29,31 @@ def _convert_to_degrees(value):
     return d + (m / 60.0) + (s / 3600.0)
 
 
-def get_exif_date_location(exif_data):
+def get_exif_location(exif_data):
     """
     Returns the latitude and longitude, if available, from the provided exif_data (obtained through get_exif_data above)
     """
     lat = None
-    lon = None
-    datetime = None
+    lng = None
 
     gps_latitude = _get_if_exist(exif_data, 'GPS GPSLatitude')
     gps_latitude_ref = _get_if_exist(exif_data, 'GPS GPSLatitudeRef')
     gps_longitude = _get_if_exist(exif_data, 'GPS GPSLongitude')
     gps_longitude_ref = _get_if_exist(exif_data, 'GPS GPSLongitudeRef')
 
-    datetime = _get_if_exist(exif_data, 'Image DateTime')
-
     if gps_latitude and gps_latitude_ref and gps_longitude and gps_longitude_ref:
         lat = _convert_to_degrees(gps_latitude)
         if gps_latitude_ref.values[0] != 'N':
             lat = 0 - lat
 
-        lon = _convert_to_degrees(gps_longitude)
+        lng = _convert_to_degrees(gps_longitude)
         if gps_longitude_ref.values[0] != 'E':
-            lon = 0 - lon
+            lng = 0 - lng
 
         lat = round(lat, 6)
-        lon = round(lon, 6)
+        lng = round(lng, 6)
 
-    return datetime, lat, lon
+    return lat, lng
 
 
 def get_exif_orientation(exif_data):
@@ -81,15 +75,15 @@ def get_exif_orientation(exif_data):
 def create_thumbnail(photo, thumbnail, size, orientation):
     with open(photo, 'rb') as photo_file:
         im = Image.open(photo_file)
-
-        if 1 in orientation.values:
-            pass
-        elif 3 in orientation.values:
-            im = im.rotate(180, expand=True)
-        elif 6 in orientation.values:
-            im = im.rotate(270, expand=True)
-        elif 8 in orientation.values:
-            im = im.transpose(Image.ROTATE_90)
+        if orientation:
+            if 1 in orientation.values:
+                pass
+            elif 3 in orientation.values:
+                im = im.rotate(180, expand=True)
+            elif 6 in orientation.values:
+                im = im.rotate(270, expand=True)
+            elif 8 in orientation.values:
+                im = im.transpose(Image.ROTATE_90)
 
         im.thumbnail(size, Image.ANTIALIAS)
         im.save(thumbnail, "JPEG", quality=100)
@@ -97,7 +91,7 @@ def create_thumbnail(photo, thumbnail, size, orientation):
 
 def get_exif_datetime(exif_data):
     photo_datetime = _get_if_exist(exif_data, 'Image DateTime')
-    return str(photo_datetime)
+    return photo_datetime
 
 
 def convert_to_timestamp(date_time: str):

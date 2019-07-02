fe308912ec4b71a2b8b7450fc0b0241e5fc8cd3b
Lagunov-PRO-Mac <alexey@lagunov.pro>
2019-07-03 00:24:24 +0300

    Added new parameters to the request

--- app/geocoder.py
+++ app/geocoder.py
@@ -9,7 +9,9 @@ def get_json_from_yandex(address: str):
     yandex_api_params = {
         'apikey': apikey,
         'format': 'json',
-        'geocode': address
+        'geocode': address,
+        'kind': 'locality',
+        'sco': 'longlat'
     }
 
     r = requests.get(url=yandex_maps_url, params=yandex_api_params)
@@ -25,10 +27,16 @@ def get_formatted_address(address: str):
     return formatted_address
 
 
-def get_area_name(address: str):
-    info = get_json_from_yandex(address)
-    area_name = \
-    info['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty']['GeocoderMetaData']\
-        ['AddressDetails']['Country']['AdministrativeArea']['AdministrativeAreaName']
-
+def get_area_name(geocoder_info):
+    area_name = geocoder_info['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty']\
+                ['GeocoderMetaData']['AddressDetails']['Country']['AdministrativeArea']['AdministrativeAreaName']
     return area_name
+
+
+def get_country_name(geocoder_info):
+    country_name = geocoder_info['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']\
+        ['metaDataProperty']['GeocoderMetaData']['AddressDetails']['Country']['CountryName']
+    return country_name
+
+# def check_country(country_name, options.)
+
